943674c29723712135df53067264dfd11d51fc9e
povkub@gmail.com <povkub@gmail.com>
2020-01-30 13:17:04 +0000

    removing dataset zip

--- scrapeIMDBforBudget.py
+++ scrapeIMDBforBudget.py
@@ -11,23 +11,24 @@ run = True
 with open('/Users/Povilas/Desktop/Final-Year-Project/datasets/movies_without_budget.csv', mode='r',  encoding="utf8") as cmdb:
         movies = csv.reader(cmdb, delimiter=";", quotechar='"')
 
-        with open('/Users/Povilas/Desktop/Final-Year-Project/datasets/movies_with_scrapped_budget.csv', 'w', newline='') as csvfile:
+        with open('/Users/Povilas/Desktop/Final-Year-Project/datasets/DEMO_movies_with_scrapped_budget.csv', 'w', newline='') as csvfile:
             fw = csv.writer(csvfile, delimiter=',',quotechar='"')
-
-            # q = Queue()
-            # lock = threading.Lock()
 
             def getBudgetFromIMDB(movieID):
                 try:
                     url = 'https://www.imdb.com/title/' + movieID
+                    # make the request to IDMb websoite to get the HTML for the movie page
                     response = requests.get(url)
+                    # using BeautifulSoup find the location of the budget in the HTML page
                     soup = BeautifulSoup(response.text, 'lxml')
                     budget_html = soup.find(text='Budget:').parent.parent
                     budget_rawval = budget_html.find('h4').next_sibling
                     budget = ''
+                    #extracting the number value from the tags that hold the budget for the movie
                     for n in budget_rawval:
                         if(n.isdigit()):
                             budget = budget + n
+                    #which thread synchronisation, add the movie budget to the dataset
                     with lock:
                         fw.writerow([movieID, budget])
                         print('Added budget for ' + movieID)
@@ -36,18 +37,21 @@ with open('/Users/Povilas/Desktop/Final-Year-Project/datasets/movies_without_bud
                     #print("No budget found for " + movieID)
                     pass
 
-
+            # defintion of the thread
+            # gets movie ID from the queue and makes the HTML request for it
             def threader():
                 while run:
                     movieID = q.get()
                     getBudgetFromIMDB(movieID)
                     q.task_done()
 
+            # initalsing 256 threads
             for x in range(256):
                 t = threading.Thread(target = threader)
                 t.daemon = True
                 t.start()
 
+            #loop to place all movie IDs of movies without budget to the queue
             for row in movies:
                 q.put(row[0])
                 #print(row[0])
